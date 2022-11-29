8b88ecaa885f2efe090d8e32e4b28c71b8ada46f
michelleuddin <michelleuddin@arizona.edu>
2022-11-29 11:09:46 -0700

    Removed commented chunks of old code

--- Final Project/src/Tag.java
+++ Final Project/src/Tag.java
@@ -1,16 +1,7 @@
 
 public class Tag {
 	private String info;
-/*<<<<<<< HEAD
-    
-    public Tag(String info) {
-        this.info = info;
-    }
-    
-    public void setInfo(String info) { this.info = info; }
-    public String getTitle() { return this.info; }
-=======
-	*/
+
 	public Tag(String info) {
 		this.info = info;
 	}
@@ -18,5 +9,4 @@ public class Tag {
 	public void setInfo(String info) { this.info = info; }
 	public String getTitle() { return this.info; }
 
-//>>>>>>> refs/remotes/origin/main
 }
